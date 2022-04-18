99af143cdcb12f96e15968beeafc73aa73ee9908
Walid boufroune <81040703+walidboufroun@users.noreply.github.com>
2022-04-18 23:05:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Le premier site sur heroku</h1>
+    <?php echo 'Le site est bien sur heroku !'; ?>
+    <a href="admin.php"><button>page admin</button></a>
+</body>
+</html>
